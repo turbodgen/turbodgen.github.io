f279b13bb2dfab7702f495d63e6187242b2ec3ca
turbo <87042592+turbodgen@users.noreply.github.com>
2022-05-19 17:39:55 -0500

    js test

--- README.md
+++
@@ -1,2 +0,0 @@
-# Page-Test
-just a test for github pages
---
+++ index.js
@@ -0,0 +1,10 @@
+let theCount = document.getElementById("count-el")
+let count = 0
+
+console.log(count)
+
+function addTo(){
+    count = count + 1
+    theCount.innerText = count
+    console.log("button was clicked my guy"+count)
+}
