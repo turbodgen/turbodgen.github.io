0a36f26783d27a50e945d4485488026c7ee53c7a
turbo <87042592+turbodgen@users.noreply.github.com>
2022-05-19 18:05:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let theCount = document.getElementById("count-el")
 let count = 0
 
-console.log(count)
+// console.log(count)
 
 function addTo(){
     count = count + 1
     theCount.innerText = count
-    console.log("button was clicked my guy"+count)
+    // console.log("button was clicked my guy"+count)
 }
