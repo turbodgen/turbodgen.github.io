bf8a457b8ad290624d5ee80e68be15f52bb67e61
turbo <87042592+turbodgen@users.noreply.github.com>
2022-05-19 22:26:05 -0500

    sidebar test

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let theCount = document.getElementById("count-el")
-let count = 0
+let theCount = document.getElementById("count-el");
+let count = 0;
 
-// console.log(count)
+// console.log(count);
 
 function addTo(){
-    count = count + 1
-    theCount.innerHTML = count
-    document.title = "testing "+count
-    console.log("button was clicked my guy"+count)
-}
+    count = count + 1;
+    theCount.innerHTML = count;
+    document.getElementById("screenDimentions").innerHTML = screen.width+"x"+screen.height;
+    document.title = "testing "+count;
+    console.log("button was clicked my guy"+count);
+};
