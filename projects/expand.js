7880382c4bf67e56bdb60148504ab4dc1a35c684
turbo <87042592+turbodgen@users.noreply.github.com>
2022-06-17 19:36:58 -0500

    show papersort notebook

--- projects/expand.js
+++ projects/expand.js
@@ -1,5 +1,7 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
+var page = 1;
+var pageImg = document.getElementById("pgShow");
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
@@ -12,3 +14,25 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 } 
+
+var theCount = document.getElementById("count-el");
+var count = 0;
+
+// console.log(count);
+
+function left(){
+  if (page<2){
+    return
+  }
+  page = page - 1;
+  pageImg.src = "psnotebook/"+page+"crop.png"
+};
+
+
+function right(){
+  if (page>3){
+    return
+  }
+  page = page + 1;
+  pageImg.src = "psnotebook/"+page+"crop.png"
+};
